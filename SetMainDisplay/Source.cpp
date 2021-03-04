af7c6c147d616c5c765857eca0dbf1c7b45980a9
Adam Patenaude <adamp.1@hotmail.com>
2021-03-04 00:11:27 -0500

    Some refactoring. Added comments.

--- SetMainDisplay/Source.cpp
+++ SetMainDisplay/Source.cpp
@@ -5,91 +5,90 @@
 
 int main()
 {
+	//WinAPI reference Parameters
 	DWORD DispNum = 0;
 	DISPLAY_DEVICE DisplayDevice;
 	DEVMODE displayInfo;
+
 	int i = 0;
 	std::vector<Screen> Screens;
-	int newDefaultDeviceNum = -1;
-	int itDoesntWorkOtherwise;
+	int newMainDeviceNum = -1;
+	int itDoesntWorkOtherwise = 0;
 	POINT cursor;
+	bool withinX, withinY;
 
 	GetCursorPos(&cursor);
 
-	// initialize DisplayDevice
+	//initialize displayInfo and DisplayDevice
+	ZeroMemory(&displayInfo, sizeof(DEVMODE));
+	displayInfo.dmSize = sizeof(DEVMODE);
 	ZeroMemory(&DisplayDevice, sizeof(DisplayDevice));
 	DisplayDevice.cb = sizeof(DisplayDevice);
 
+	//establish new main display
+	//iterates over each display device in the current session until it fails when DispNum is greater than the greatest registered display index
+	//information about the current iterated display into DisplayDevice
 	while (EnumDisplayDevices(NULL, DispNum, &DisplayDevice, 0)) {
-		ZeroMemory(&displayInfo, sizeof(DEVMODE));
-		displayInfo.dmSize = sizeof(DEVMODE);
+
+		//retrieves more information about the current iterated display into displayInfo using DisplayDevice.DeviceName
 		EnumDisplaySettings((LPCWSTR)DisplayDevice.DeviceName, ENUM_REGISTRY_SETTINGS, &displayInfo);
 
-		if ((DisplayDevice.StateFlags & DISPLAY_DEVICE_ATTACHED_TO_DESKTOP)) {
+		//DISPLAY_DEVICE_ACTIVE is the same as DISPLAY_DEVICE_ATTACHED_TO_DESKTOP refered to in the WinAPI documentation
+		if ((DisplayDevice.StateFlags & DISPLAY_DEVICE_ACTIVE)) {
 			Screens.push_back(Screen(displayInfo.dmPosition.x, displayInfo.dmPosition.y, displayInfo.dmPelsWidth, displayInfo.dmPelsHeight, DisplayDevice.DeviceName));
 
+			//line below does not work for some reason
+			//withinX = displayInfo.dmPosition.x < cursor.x and cursor.x < (displayInfo.dmPosition.x + displayInfo.dmPelsWidth);
 			itDoesntWorkOtherwise = displayInfo.dmPosition.x + displayInfo.dmPelsWidth;
-			bool withinX = displayInfo.dmPosition.x < cursor.x and cursor.x < itDoesntWorkOtherwise;
+			withinX = displayInfo.dmPosition.x < cursor.x and cursor.x < itDoesntWorkOtherwise;
 			itDoesntWorkOtherwise = displayInfo.dmPosition.y + displayInfo.dmPelsHeight;
-			bool withinY = displayInfo.dmPosition.y < cursor.y and cursor.y < itDoesntWorkOtherwise;
+			withinY = displayInfo.dmPosition.y < cursor.y and cursor.y < itDoesntWorkOtherwise;
 
-			newDefaultDeviceNum = withinX and withinY and !(DisplayDevice.StateFlags & DISPLAY_DEVICE_PRIMARY_DEVICE) ? i : newDefaultDeviceNum;
+			//device in cursor position 
+			//~ used isntead of ! for flags according to C6317
+			if(withinX and withinY and (DisplayDevice.StateFlags & ~DISPLAY_DEVICE_PRIMARY_DEVICE & ~DISPLAY_DEVICE_REMOVABLE))
+				newMainDeviceNum =  i;
 		}
 
+		//reset displayInfo and DisplayDevice
+		ZeroMemory(&displayInfo, sizeof(DEVMODE));
+		displayInfo.dmSize = sizeof(DEVMODE);
 		ZeroMemory(&DisplayDevice, sizeof(DisplayDevice));
 		DisplayDevice.cb = sizeof(DisplayDevice);
+
 		DispNum++;
 		i++;
 	}
 
-	if (newDefaultDeviceNum >= 0) {
-		Screen newDefaultScreen = Screens.at(newDefaultDeviceNum);
+	//repositions displays around new main display
+	if (newMainDeviceNum >= 0) {
+		Screen newMainScreen = Screens.at(newMainDeviceNum);
 		DispNum = 0;
 		i = 0;
+
 		while (EnumDisplayDevices(NULL, DispNum, &DisplayDevice, 0)) {
 			ZeroMemory(&displayInfo, sizeof(DEVMODE));
 			displayInfo.dmSize = sizeof(DEVMODE);
 
 			EnumDisplaySettings((LPCWSTR)DisplayDevice.DeviceName, ENUM_REGISTRY_SETTINGS, &displayInfo);
 			
-			if ((DisplayDevice.StateFlags & DISPLAY_DEVICE_ATTACHED_TO_DESKTOP)) {
+			if ((DisplayDevice.StateFlags & DISPLAY_DEVICE_ACTIVE)) {
 
 				Screen currentScreen = Screens.at(i);
-
 				displayInfo.dmFields = DM_POSITION;
 
-				//x
-				if (i == newDefaultDeviceNum) {
+				//new main display, set 0,0
+				if (i == newMainDeviceNum) {
 					displayInfo.dmPosition.x = 0;
 					displayInfo.dmPosition.y = 0;
 					ChangeDisplaySettingsEx(DisplayDevice.DeviceName, &displayInfo, NULL, CDS_UPDATEREGISTRY | CDS_SET_PRIMARY | CDS_NORESET, NULL);
-					ZeroMemory(&DisplayDevice, sizeof(DisplayDevice));
-					DisplayDevice.cb = sizeof(DisplayDevice);
-					DispNum++;
-					i++;
-					continue;
 				}
-				else if (currentScreen.X == newDefaultScreen.X) {
-					displayInfo.dmPosition.x = 0;
-				}
-				else if (currentScreen.X < newDefaultScreen.X) {
-					displayInfo.dmPosition.x = currentScreen.X - newDefaultScreen.X;
-				}
-				else if (currentScreen.X > newDefaultScreen.X) {
-					displayInfo.dmPosition.x = currentScreen.X - newDefaultScreen.X;
-				}
-				//y
-				if (currentScreen.Y == newDefaultScreen.Y) {
-					displayInfo.dmPosition.y = 0;
-				}
-				else if (currentScreen.Y < newDefaultScreen.Y) {
-					displayInfo.dmPosition.y = currentScreen.Y - newDefaultScreen.Y;
-				}
-				else if (currentScreen.Y > newDefaultScreen.Y) {
-					displayInfo.dmPosition.y = currentScreen.Y - newDefaultScreen.Y;
-				}
-				
-				ChangeDisplaySettingsEx(DisplayDevice.DeviceName, &displayInfo, NULL, CDS_UPDATEREGISTRY | CDS_NORESET, NULL);
+				//set position relative to new main display
+				else {
+					displayInfo.dmPosition.x = currentScreen.X - newMainScreen.X;
+					displayInfo.dmPosition.y = currentScreen.Y - newMainScreen.Y;
+					ChangeDisplaySettingsEx(DisplayDevice.DeviceName, &displayInfo, NULL, CDS_UPDATEREGISTRY | CDS_NORESET, NULL);
+				}	
 			}
 
 			ZeroMemory(&DisplayDevice, sizeof(DisplayDevice));
@@ -98,5 +97,7 @@ int main()
 			i++;
 		}
 	}
+
+	//apply changes
 	ChangeDisplaySettingsEx(NULL, NULL, NULL, NULL, NULL);
 }
